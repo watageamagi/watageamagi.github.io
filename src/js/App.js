de69f1c59c99568b50f577aa28e974a62287b225
1qwq1-1945 <takaura@shrp.jp>
2021-01-11 16:25:24 +0900

    ジャンケン

--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,14 @@
+const JANKEN_GU = 0;
+const JANKEN_TYOKI = 1;
+const JANKEN_PA = 2;
+const JANKEN_LIST = [
+    JANKEN_GU,
+    JANKEN_TYOKI,
+    JANKEN_PA
+]
+// const JANKEN_LIST = {
+//     JANKEN_GU:0,JANKEN_TYOKI:1,JANKEN_PA:2
+// }
 
 const vue = new Vue({
     el: '#app',
@@ -6,6 +17,9 @@ const vue = new Vue({
         return {
             isGameStart: false,
             isGameOver: false,
+            playertehuda:null,
+            cputehuda:null,
+            kekka:''
         }
     },
 
@@ -21,6 +35,50 @@ const vue = new Vue({
 
         restart() {
             this.isGameOver = false
+        },
+
+        tehudachange(value){
+            this.playertehuda =value
+            // if(this.playertehuda === 'gu'){
+            //     this.cputehuda ='pa'
+            // }else if(this.playertehuda === 'tyoki'){
+            //     this.cputehuda ='gu'
+            // }else if(this.playertehuda === 'pa'){
+            //     this.cputehuda ="tyoki"
+            // }
+
+            this.cputehuda =JANKEN_LIST[Math.floor(Math.random() * JANKEN_LIST.length)];
+
+            if((this.playertehuda - this.cputehuda + 3)%3 === 2){
+                this.kekka = "勝ち"
+            }else if((this.playertehuda - this.cputehuda + 3)%3 === 1){
+                this.kekka ="負け"
+            }else{
+                this.kekka ="引き分け"
+            }
+        }
+    },
+
+    computed:{
+        getTehudaName() {
+            switch (this.playertehuda) {
+                case JANKEN_GU:
+                    return 'gu'
+                case JANKEN_TYOKI:
+                    return 'tyoki'
+                case JANKEN_PA:
+                    return 'pa'
+            }
+        },
+        getCpuName() {
+            switch (this.cputehuda) {
+                case JANKEN_GU:
+                    return 'gu'
+                case JANKEN_TYOKI:
+                    return 'tyoki'
+                case JANKEN_PA:
+                    return 'pa'
+            }
         }
     }
 })
